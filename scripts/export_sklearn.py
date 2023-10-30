ef71269ff5e30bcef99454126f537dd69f62f272
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-10-30 18:39:29 +0100

    Add training scripts

---
+++ scripts/export_sklearn.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from skl2onnx import to_onnx
+import os
+
+np.random.seed(101)
+data = np.random.randn(1000, 3).astype(np.float32)
+y = (data.sum(1) / 2).astype(int)
+y = y - np.min(y)
+print(sorted(np.unique(y)))
+
+black_op = ["ZipMap"]
+clf = DecisionTreeClassifier()
+clf.fit(data, y)
+onx = to_onnx(clf, data[:1, :], target_opset=12, black_op=black_op)
+
+
+with open("model/model.onnx", "wb") as f:
+    f.write(onx.SerializeToString())
+
+os.system(
+    "python -m onnxruntime.tools.make_dynamic_shape_fixed --input_name X --input_shape 1,3  model/model.onnx model/model.onnx"
+)
---
+++ scripts/export_torch_mlp.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+np.random.seed(101)
+data = np.random.randn(1000, 3).astype(np.float32)
+y = (data.sum(1) / 2).astype(int)
+y = y - np.min(y)
+print(sorted(np.unique(y)))
+
+
+data = torch.from_numpy(data)
+y = torch.from_numpy(y)
+
+model = nn.Sequential(
+    nn.Linear(3, 5),
+    nn.ReLU(),
+    nn.Linear(5, 6),
+)
+print(model)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+for n in range(100):
+    y_pred = model(data)
+    loss = loss_fn(y_pred, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+with open("model/model.onnx", "wb") as f:
+    torch.onnx.export(model, torch.randn(1, 3), f)
